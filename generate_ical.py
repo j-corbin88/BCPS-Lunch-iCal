0fcc289cc20febf167e77fd90dce27e9fca8c2c2
j-corbin88 <jordan.j.corbin@gmail.com>
2026-05-18 10:31:14 -0400

    Update generate_ical.py

--- generate_ical.py
+++ generate_ical.py
@@ -23,7 +23,7 @@ BREAKFAST_KEYWORDS = [
     "waffle", "pancake", "muffin", "bagel", "cereal", "oatmeal",
     "granola", "french toast", "breakfast", "biscuit", "donut",
     "pop tart", "cinnamon", "cocoa puff", "lucky charm", "cheerio",
-    "fruit juice", "juice", "yogurt", "egg"
+    "fruit juice", "juice", "yogurt", "egg", "mini cinnis"
 ]
 
 
